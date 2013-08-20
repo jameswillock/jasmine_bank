11eebc9142585f736e60f07f024c14f1dc52a439
James Willock <james.willock@gmail.com>
2013-08-20 11:57:02 +0100

    Fixed defaults

--- js/lib/Account.js
+++ js/lib/Account.js
@@ -9,7 +9,7 @@ var Account = function(options) {
     startingBalance: 0
   };
 
-  this.balance = typeof options.balance !== 'undefined' ? options.balance : defaults.balance;
+  this.balance = typeof options.startingBalance !== 'undefined' ? options.startingBalance : defaults.startingBalance;
 
 };
 
