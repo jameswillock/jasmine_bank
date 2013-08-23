2b94bf7262ce12dda8fa8f4c5678d6d18bc7fe78
James Willock <james.willock@gmail.com>
2013-08-23 10:28:40 +0100

    Cleared up for graduates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-var account = new Account({
-  startingBalance: 200
-});
-
 window.onload = function() {
-  new AccountForm({
-    depositForm: document.getElementById("deposit_form"),
-    withdrawForm: document.getElementById("withdraw_form"),
-    account: account
-  });
+  console.log("Hello there, graduates.");
 };
